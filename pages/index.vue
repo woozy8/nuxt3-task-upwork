eafa928cd1ad38f3697de043978819c6d3bd42fb
dymitr <woozywonder7@gmail.com>
2024-01-28 14:17:38 +0100

    Replace cards data mock to entities folder.

--- pages/index.vue
+++ pages/index.vue
@@ -31,62 +31,11 @@
 </template>
 
 <script setup lang="ts">
+import cardsData from '@/entities/cards.json'
+
 useHead({
   title: 'Nuxt 3 Test Task',
 })
 
-const cards = ref([
-  {
-    id: 1,
-    title: 'Photography',
-    benefits: ['Drone Photography', 'Interiors', 'Exteriors / Architectural'],
-    description:
-      'Every photograph we produce finds the beauty in your property while serving as a profitable asset.',
-    images: ['camera', 'drone-view'],
-  },
-  {
-    id: 2,
-    title: 'Virtual Staging',
-    benefits: null,
-    description:
-      'Our staging will help you sell the potential of your space. We can work off of our own photography or photos you provide.',
-    images: ['interior2'],
-  },
-  {
-    id: 3,
-    title: 'Renderings',
-    benefits: ['Interiors', 'Exteriors'],
-    description:
-      'Renderings are the gold standard in pre-construction marketing.',
-    images: ['interior'],
-  },
-  {
-    id: 4,
-    title: 'Interactive Tours',
-    benefits: ['Matterport', 'Video Walkthroughs'],
-    description:
-      'Give your audience the ability to explore your property without requiring in-person travel.',
-    images: ['exterior'],
-  },
-  {
-    id: 5,
-    title: 'Floor Plans',
-    benefits: ['On-ite Measure', '2D Floor Plans', '3D Floor Plans'],
-    description:
-      'We produce laser-precise floor plans faster than any other service.',
-    images: ['house-model3d'],
-  },
-  {
-    id: 6,
-    title: 'Video',
-    benefits: [
-      'Drone Cinematography',
-      'Interiors',
-      'Exteriors / Architectural',
-    ],
-    description:
-      'Video content has higher engagement and will help your property stand out online.',
-    images: ['horses'],
-  },
-])
+const cards = ref(cardsData)
 </script>
